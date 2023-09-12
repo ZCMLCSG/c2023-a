272feb3915f793e0889f4cbd4742e3e21d388055
ZCMLCSG <3160789235@qq.com>
2023-09-13 00:15:32 +0800

    从2到n-1一个个验证是否是n的因数

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,18 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("hello world!\n");
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=2;i<=n-1;i++)
+    {
+        if(n%i==0)
+            break;
+    }
+    if(i>=n){
+        printf("n is a prime\n");
+    }else
+        printf("n is not a prime");
     return 0;
 }
