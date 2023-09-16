4fbc023737c96342457a19a6fde73a42c8c9e609
ZCMLCSG <144547132+ZCMLCSG@users.noreply.github.com>
2023-09-16 19:40:08 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+    int i,a;
+    double start,finish;
+    start=clock();
+    for(a=2;a<=1000;a++)
+    {
+        for(i=2;i<=a-1;i++)
+        {
+            if(a%i==0)
+                break;
+        }
+        if(i>=a)
+        {
+            printf("%d\n",a);
+        }
+    }
+    finish=clock();
+    printf("%f seconds\n",(finish-start)/CLOCKS_PER_SEC);
 }
