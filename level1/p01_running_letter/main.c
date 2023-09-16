353ed9dcfdb97b52281935ba4151edfa2aeccf61
ZCMLCSG <144547132+ZCMLCSG@users.noreply.github.com>
2023-09-16 22:40:45 +0800

    Update main.c

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,25 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include <stdlib.h>
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+    int i;
+    int x=0;
+    int a=30;
+    int b=1;
+    while(1)
+    {
+        x=x+b;
+        system("cls");
+        for (i=0;i<x;i++)
+        {
+            printf(" ");
+        }
+        printf("A");
+        Sleep(50);
+        if(x==a)
+            b=-b;
+        if(x==0)
+            b=-b;
+    }
 }
