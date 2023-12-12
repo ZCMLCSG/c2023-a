34c3294e62377079b0e5d9503fdc6d115a11735a
ZCMLCSG <144547132+ZCMLCSG@users.noreply.github.com>
2023-12-12 19:30:35 +0800

    Update main.c

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,28 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include<stdio.h>
+#include<windows.h>
+void move(char A,char C)
+{
+    printf("%c->%c\n",A,C);
+}
+void HanoTower(char A,char B,char C,int n)
+{
+    if(n==1)
+    {
+        move(A,C);
+    }
+    else
+    {
+        HanoTower(A,C,B,n-1);
+        move(A,C);
+        HanoTower(B,A,C,n-1);
+    }
+}
+int main()
+{
+    int n=0;
+    printf("请输入A柱子上圆盘的个数：\n");
+    scanf("%d",&n);
+    HanoTower('A','B','C',n);
+    Sleep(5000);
     return 0;
 }
