87ba2d407392d73f29d94d9e32355ba8cb96c704
ZCMLCSG <144547132+ZCMLCSG@users.noreply.github.com>
2023-12-12 19:29:07 +0800

    Update main.c

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,55 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include<string.h>
+#include<stdio.h>
+#include<windows.h>
+void encryption(char a[],int d)
+{
+    int c=strlen(a);
+    for(int i=0;i<c;i++)
+    {
+        a[i]=a[i]+d;
+    }
+    printf("加密后的密文为:\n");
+    for(int i=0;i<c-1;i++)
+    {
+        printf("%c",a[i]);
+    }
+    printf("%c\n",a[c-1]);
+}
+void decrypt(char a[],int k)
+{
+    int c=strlen(a);
+    for(int i=0;i<c;i++)
+    {
+        a[i]=a[i]-k;
+    }
+    printf("解密后的明文为:");
+    for(int i=0;i<c-1;i++)
+    {
+        printf("%c",a[i]);
+    }
+    printf("%c\n",a[c-1]);
+}
+int main()
+{
+    char b;
+    char a[100];
+    printf("请输入明文:");
+    gets(a);
+    int d;
+    printf("请设定密钥(数字):");
+    scanf("%d",&d);
+    encryption(a,d);
+    int k;
+    printf("请输入密钥:");
+    scanf("%d",&k);
+    if(d==k)
+    {
+        decrypt(a,d);
+        Sleep(3000);
+    }
+    else
+    {
+        printf("密钥错误！\n");
+        Sleep(3000);
+    }
 }
